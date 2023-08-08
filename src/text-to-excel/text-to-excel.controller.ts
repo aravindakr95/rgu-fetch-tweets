a26fdea4f898eacf9f99068555d69205afa0183d
Aravinda Rathnayake <aravindar@fortude.co>
2023-08-08 06:12:54 +0530

    Bug fixes

--- src/text-to-excel/text-to-excel.controller.ts
+++ src/text-to-excel/text-to-excel.controller.ts
@@ -3,6 +3,7 @@ import { GlobalAnyResponse, TextToExcelRequest } from '../models';
 import * as fs from 'fs';
 import * as excelJs from 'exceljs';
 import * as path from 'path';
+import {rimraf} from 'rimraf';
 
 @Controller('text-to-excel')
 export class TextToExcelController {
@@ -15,20 +16,25 @@ export class TextToExcelController {
   async convertTextToExcel(@Body() req: TextToExcelRequest): Promise<GlobalAnyResponse> {
     try {
       const workbook = new excelJs.Workbook();
-      const worksheet = workbook.addWorksheet('');
+      const worksheet = workbook.addWorksheet(req.worksheetName);
+      const columnKey = req.columnName.toLowerCase();
+      rimraf.sync(req.excelFilePath);
 
-      worksheet.columns = [{ header: req.columnName, key: req.columnName.toLowerCase() }];
+      worksheet.columns = [{ header: req.columnName, key: columnKey }];
       const fileNames = await fs.promises.readdir(req.folderPath);
+
+      console.log(fileNames)
 
       for (const fileName of fileNames) {
         if (fileName.endsWith('.txt')) {
           const filePath = path.join(req.folderPath, fileName);
           const content = await fs.promises.readFile(filePath, 'utf-8');
-          worksheet.addRow({ content });
+          worksheet.addRow({ [columnKey]: content });
         }
       }
 
-      await workbook.xlsx.writeFile(req.excelFilePath);
+      const res = await workbook.xlsx.writeFile(req.excelFilePath);
+      console.log(res)
       console.log(`Excel file "${req.excelFilePath}" saved successfully.`);
 
       return { status: true, statusCode: 200, data: null, internalMessage: 'Text to Excel conversion is completed and saved' };
